88b4a674e5b2244269e7aa0ccf63ac4795f6d53f
olivia pasion <oap9820@gmail.com>
2022-08-09 14:54:08 -0700

    #POST create user passes

---
+++ __tests__/user.test.js
@@ -0,0 +1,45 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+const testUser = {
+  firstName: 'jane',
+  lastName: 'Doe',
+  email: 'abc@123.com',
+  password: 'abc123'
+};
+
+const registerAndLogin = async (userProps = {})  => {
+  const password = userProps.password ?? testUser.password;
+
+  const agent = request.agent(app);
+  
+  const user = await UserService.create({ ...testUser, ...userProps });
+
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  console.log({ user });
+  return [agent, user];
+};
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('registers a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(testUser);
+    const { firstName, lastName, email } = testUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
